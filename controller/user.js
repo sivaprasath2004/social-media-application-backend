26e6bb0218535284c477e4416b447c304ad83e6e
Dark <prasathsiva2004@gmail.com>
2024-03-24 20:51:04 +0530

    login & signup

--- controller/user.js
+++ controller/user.js
@@ -4,10 +4,57 @@ const login_schema=require('../mongodb/loginschema')
 const login=async(req,res)=>{
    try {
    await mongoose.connect(process.env.DB);
-   console.log(req,body)
+   let val= await login_schema.findOne({Email_id:req.body.email}) 
+   if(val){
+    if(val.pass===req.body.pass){
+   res.status(200).send({res:'ok',user:val})
+    }
+    else{
+    res.status(200).send({res:"error",user:'Invalid Password'})
+    }
+   }
+   else{
+    res.status(200).send({res:"error",user:'No more user found'})
+   }
 } catch (error) {
   console.error('Error connecting to MongoDB:', error);
 }
-  
+  finally{
+    await mongoose.disconnect()
+  }
 }
-module.exports={login}
+const signup=async(req,res)=>{
+  try {
+     await mongoose.connect(process.env.DB);
+     if(req.body.name && req.body.usernsme && req.body.email && req.body.pass){
+   let val= await login_schema.findOne({Email_id:req.body.email}) 
+    if(val){
+    res.status(200).send({res:"error",user:'Already used in mail Adress'})
+    }else{
+      let username=await login_schema.findOne({user_name:req.body.username})
+      if(username){
+        res.status(200).send({res:"error",user:'Already User Name taken'})
+      }
+      else{
+      let user=await login_schema.create({
+        name:req.body.name,
+        user_name:req.body.username,
+        Email_id:req.body.email,
+        Des:req.body.Des,
+        pass:req.body.pass
+      })
+      res.status(200).send({res:"ok",user:user})
+    }
+  }
+  }
+  else{
+    res.status(200).send({res:"error",user:'error'})
+  }
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+    finally{
+      await mongoose.disconnect()
+    }
+}
+module.exports={login,signup}
--- mongodb/loginschema.js
+++ mongodb/loginschema.js
@@ -1,10 +1,9 @@
 const mongoose=require('mongoose')
 let login_schema=new mongoose.Schema({
   name:String,
-  dob:String,
-  ph_no:Number,
-  gender:String,
+  user_name:String,
   Email_id:String,
+  Des:String,
   pass:String
 })
 module.exports=mongoose.model('login',login_schema)
