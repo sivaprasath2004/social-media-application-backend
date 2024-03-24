88a66d85156c061a61338d9eb3752a2f6435807b
Dark <prasathsiva2004@gmail.com>
2024-03-24 08:56:04 +0530

    env

--- controller/user.js
+++ controller/user.js
@@ -4,6 +4,7 @@ const login_schema=require('../mongodb/loginschema')
 const login=async(req,res)=>{
    try {
    await mongoose.connect(process.env.DB);
+   console.log(req,body)
 } catch (error) {
   console.error('Error connecting to MongoDB:', error);
 }
