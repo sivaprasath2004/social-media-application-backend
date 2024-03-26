727515058fa50f68a6106403ba893d3b0b714309
sivaprasath2004 <prasathsiva2004@gmail.com>
2024-03-26 20:38:11 +0530

    schema

--- controller/user.js
+++ controller/user.js
@@ -27,8 +27,9 @@ async function searchResult(req,res){
   console.log(req.body)
   try {
     await mongoose.connect(process.env.DB);
-    let val= await login_schema.find({user_name:req.body.val}) 
-    console.log(val)
+    const regex = new RegExp(`.*${req.body.val}.*`, 'i')
+    let val= await login_schema.find({user_name:regex}) 
+    res.status(200).send(val)
   } catch (error) {
    console.error('Error connecting to MongoDB:', error);
  }
@@ -39,8 +40,9 @@ async function searchResult(req,res){
 const signup=async(req,res)=>{
   try {
      await mongoose.connect(process.env.DB);
-     if(req.body.name && req.body.usernsme && req.body.email && req.body.pass){
+     if(req.body.name && req.body.username && req.body.email && req.body.pass){
    let val= await login_schema.findOne({Email_id:req.body.email}) 
+   console.log(val)
     if(val){
     res.status(200).send({res:"error",user:'Already used in mail Adress'})
     }else{
@@ -70,4 +72,32 @@ const signup=async(req,res)=>{
       await mongoose.disconnect()
     }
 }
-module.exports={login,signup,searchResult}
+const followers=async(me,you,name)=>{
+  try{
+    console.log(me,you,name)
+    await mongoose.connect(process.env.DB);
+    let chattingId=me+you
+    let first=await login_schema.findById(me)
+    let following=first.following.filter(item=>item!==you)
+    let fir_messages=first.messages.filter(item=>item!==chattingId)
+    fir_messages.push(chattingId)
+    following.push(you)
+    first.messages=fir_messages
+    first.following=following
+    let second=await login_schema.findById(you)
+    let followers=second.followers.filter(item=>item!==me)
+    let sec_messages=second.messages.filter(item=>item!==chattingId)
+    sec_messages.push(chattingId)
+    followers.push(me)
+    second.followers=followers
+    second.messages=sec_messages
+   await first.save()
+   await second.save()
+   } catch (error) {
+  console.error('Error connecting to MongoDB:', error);
+   }
+  finally{
+    await mongoose.disconnect()
+  }
+}
+module.exports={login,signup,searchResult,followers}
--- mongodb/loginschema.js
+++ mongodb/loginschema.js
@@ -4,6 +4,9 @@ let login_schema=new mongoose.Schema({
   user_name:String,
   Email_id:String,
   Des:String,
-  pass:String
+  pass:String,
+  following:[String],
+  followers:[String],
+  messages:[String]
 })
 module.exports=mongoose.model('login',login_schema)
