ed1ea43107210d4754e6e3af2100bf8faac97b23
Sivaprasath <121082414+sivaprasath2004@users.noreply.github.com>
2024-04-10 07:03:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const socketio = require("socket.io");
 const io = socketio(server, { 
   cors: { 
     origin: 'http://localhost:3000',
-    methods: ["GET", "POST"] // Optionally, specify allowed methods
+    methods: ["GET", "POST"] 
   } 
 });
 const bodyParser = require("body-parser");
